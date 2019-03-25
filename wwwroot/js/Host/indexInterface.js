a09ba2b0ba1a0ab65be03b9f0f70a885bad17d49
sg1110 <saule.guzyte@tesco.com>
2019-03-25 11:16:41 +0000

    Limits play icons to 2 selections

--- wwwroot/js/Host/indexInterface.js
+++ wwwroot/js/Host/indexInterface.js
@@ -1,19 +1,6 @@
-// $( "#create" ).click(function(event) {
-//     event.preventDefault()
-//     console.log("Hello Saule")
-//     // Change to C#, save to DB?
-//     var qArray = [
-//         document.getElementById("q1").value,
-//         document.getElementById("q2").value,
-//         document.getElementById("q3").value,
-//         document.getElementById("q4").value,
-//         document.getElementById("q5").value,
-//         document.getElementById("q6").value,
-//         document.getElementById("q7").value,
-//         document.getElementById("q8").value,
-//         document.getElementById("q9").value,
-//         document.getElementById("q10").value
-//     ]
-//     localStorage.setItem("questions", JSON.stringify(qArray));
-//     $(location).attr('href', '/Host/lobby')
-// });
+var limit = 2;
+$('input.form-check-input').on('change', function(evt) {
+   if($(this).siblings(':checked').length >= limit) {
+       this.checked = false;
+   }
+});
