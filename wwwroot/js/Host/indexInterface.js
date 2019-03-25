ae7e8eb04372b8aefb9142caedaa271931c9c09e
sg1110 <saule.guzyte@tesco.com>
2019-03-25 16:27:00 +0000

    test commit

--- wwwroot/js/Host/indexInterface.js
+++ wwwroot/js/Host/indexInterface.js
@@ -2,17 +2,17 @@
 $(document).ready(function () {
 
     var limit = 2;
-    $('input.form-check-input').on('change', function(evt) {
-    if($(this).siblings(':checked').length >= limit) {
-         this.checked = false;
-         }
+    $('input.form-check-input').on('change', function (evt) {
+        if ($(this).siblings(':checked').length >= limit) {
+            this.checked = false;
+        }
     });
 
 
-    $('#create').click(function(){
+    $('#create').click(function () {
         var selected = [];
-        $('.cc-selector-2 input:checked').each(function() {
-               selected.push($(this).attr('value'));
+        $('.cc-selector-2 input:checked').each(function () {
+            selected.push($(this).attr('value'));
         });
         console.log(selected);
     })
