9a5d70a2eee224bd8a469d51b65cc7efb9111f75
John Haythorn <johnhaythorn@yahoo.co.uk>
2019-03-26 16:19:23 +0000

    limiting votes to one per player

--- wwwroot/js/Player/voteInterface.js
+++ wwwroot/js/Player/voteInterface.js
@@ -5,6 +5,7 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/pollHub").build();
 
 // Opens Connection to Hub
 connection.start().catch(function (err) {
+    displayforVote();
     return console.error(err.toString());
 });
 
@@ -12,16 +13,32 @@ connection.start().catch(function (err) {
 // When Vote is clicked send Vote to Host
 $(document).ready(function () {
     receiveQuestionFromHost();
+    receiveIconsFromHost();
     $('#vote').on('click', function () {
         event.preventDefault();
         sendVoteToHost();
+        displayForWait();
     });
 })
 
 // Listen for Question from Host
 function receiveQuestionFromHost() {
     connection.on("ReceiveQuestion", function (question) {
+        displayforVote();
         $("#questionTitle").html(question);
+    });
+}
+
+// Listen for Icons from Host
+function receiveIconsFromHost(){
+    console.log("Receiving Icons");
+    connection.on("ReceiveIconId", function (icon1, icon2) {
+        $('#answerOne').html(`<input id="${icon1}2" class="answerOne" type="radio" name="myResponse" value="IconId1" />
+        <label class="drinkcard-cc ${icon1}" for="${icon1}2"></label>`);
+        $('#answerTwo').html(`<input id="${icon2}2" class="answerTwo" type="radio" name="myResponse" value="IconId2" />
+        <label class="drinkcard-cc ${icon2}" for="${icon2}2"></label>`);
+        console.log(icon1);
+        console.log(icon2);
     });
 }
 
@@ -31,13 +48,27 @@ function sendVoteToHost() {
     // If something is selected
     if ($('input:radio[name=myResponse]').is(':checked')) {
         // Assign Values
-        var myResponseId = $('input[name=myResponse]:checked').attr('id');
+        var myResponseId = $('input[name=myResponse]:checked').attr('class');
         var myResponseVal = $('input[name=myResponse]:checked').val();
+        console.log(myResponseId);
+        console.log(myResponseVal);
         // Broadcast to Host
         connection.invoke("SendMessage", username, myResponseId, myResponseVal).catch(function (err) {
             return console.error(err.toString());
         });
     } else {
         return console.log("No response selected.");
+    }
+}
+
+function displayForWait() {
+    if ($('#vote').css('display') != 'none') {
+        $('#waitingVote').show().siblings('div').hide();
+    }
+}
+
+function displayforVote() {
+    if ($('#vote').css('display') != 'none') {
+        $('#voteContainer').show().siblings('div').hide();
     }
 }
